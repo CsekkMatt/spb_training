611e3a71ea40acf93d6a27ad4e6a13ed6e5c2dda
Mate Petok <petok.mate14@gmail.com>
2020-10-17 19:45:45 +0300

    back-end connection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-insert into Blogger (age,name) VALUES (23,'Mate');
-insert into Blogger (age,name) VALUES (21,'Edina');
-insert into Blogger (age,name) VALUES (23,'Alpar');
-insert INTO Blogger (age,name) VALUES(24,'Alma');
+insert into Blogger (id,age,name) VALUES (2,23,'Mate');
+insert into Blogger (id,age,name) VALUES (3,21,'Edina');
+insert into Blogger (id,age,name) VALUES (4,23,'Alpar');
+insert INTO Blogger (id,age,name) VALUES (5,24,'Alma');
 
 
-insert INTO Stories (title,content,posted,blogger_id) VALUES('Teszt cim','Teszt tartalom','2020-10-14',1);
-insert INTO Stories (title,content,posted,blogger_id) VALUES('Teszt1 cim','Teszt1 tartalom',CURRENT_DATE(),(select id from Blogger where name = 'Alpar'));
-insert INTO Stories (title,content,posted,blogger_id) VALUES('Teszt2 cim','Teszt2 tartalom',CURRENT_DATE(),(select id from Blogger where name = 'Mate')); 
-insert INTO Stories (title,content,posted,blogger_id) VALUES('Szia','Teszt2 tartalom',CURRENT_DATE(),(select id from Blogger where name = 'Mate')); 
+insert INTO Stories (id,title,content,posted,blogger_id) VALUES (2,'Teszt cim','Teszt tartalom','2020-10-14',1);
+insert INTO Stories (id,title,content,posted,blogger_id) VALUES (3,'Teszt1 cim','Teszt1 tartalom','2020-10-13', (select id from Blogger where name = 'Alpar'));
+insert INTO Stories (id,title,content,posted,blogger_id) VALUES (4,'Teszt2 cim','Teszt2 tartalom','2020-10-12', (select id from Blogger where name = 'Mate')); 
+insert INTO Stories (id,title,content,posted,blogger_id) VALUES (5,'Szia','Teszt2 tartalom','2020-10-11', (select id from Blogger where name = 'Mate')); 
